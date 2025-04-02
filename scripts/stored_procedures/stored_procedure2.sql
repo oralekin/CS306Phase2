8af32ae9db3a755cb327a10467503a39f0b2875d
dimoedo <edoardocecca@gmail.com>
2025-04-02 20:20:28 +0200

    Adjust procedure

--- scripts/stored_procedures/stored_procedure2.sql
+++ scripts/stored_procedures/stored_procedure2.sql
@@ -1,7 +1,10 @@
+DELIMITER //
 CREATE PROCEDURE check_winner(IN curr_id INT)
 BEGIN
     SELECT ippon*10 + wazari*5 + yuko as Points, PS.mId  
     FROM PlayedScore PS, JudoMatch JM 
     WHERE PS.mId = 1 AND PS.forfeit <> curr_id AND PS.mId = JM.mId 
     ORDER BY Points DESC LIMIT 1;
-END;
+END //
+
+DELIMITER;
