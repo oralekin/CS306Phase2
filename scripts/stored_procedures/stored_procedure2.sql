096693d2d5ec53cecc8860a3943ceeceffda8a66
dimoedo <edoardocecca@gmail.com>
2025-04-02 21:15:44 +0200

    Added new procedure for procedure 2

--- scripts/stored_procedures/stored_procedure2.sql
+++ scripts/stored_procedures/stored_procedure2.sql
@@ -1,10 +1,16 @@
 DELIMITER //
 CREATE PROCEDURE check_winner(IN curr_id INT)
 BEGIN
-    SELECT ippon*10 + wazari*5 + yuko as Points, PS.mId  
-    FROM PlayedScore PS, JudoMatch JM 
-    WHERE PS.mId = 1 AND PS.forfeit <> curr_id AND PS.mId = JM.mId 
+    SELECT PS.mId, ippon*10 + wazari*5 + yuko as Points, ippon, wazari, yuko, J.jName   
+    FROM PlayedScore PS, JudoMatch JM, Judoka J
+    WHERE PS.mId = 1 AND PS.forfeit <> curr_id AND PS.mId = JM.mId, J.jId = PS.jId 
     ORDER BY Points DESC LIMIT 1;
 END //
 
+CREATE PROCEDURE versus
+BEGIN
+    SELECT P1.mId, J1.jName as player1, J2.jName as player2
+    FROM PlayedScore P1, PlayedScore P2, Judoka J1, Judoka J2
+    WHERE J1.jId < J2.jId AND P1.jId < P2.jId AND P1.mId = P2.mId AND P1.jId = J1.jId AND P2.jId = J2.jId
+END//
 DELIMITER;
