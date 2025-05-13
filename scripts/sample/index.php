67a1022ff7c4b03418e362634a758fa306cce0af
Ekin Oral <oralekin@sabanciuniv.edu>
2025-05-13 22:27:05 +0300

    fix path to assets

--- scripts/sample/index.php
+++ scripts/sample/index.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>LAMP STACK</title>
-        <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/svg+xml">
-        <link rel="stylesheet" href="/assets/css/bulma.min.css">
+        <link rel="shortcut icon" href="./assets/images/favicon.svg" type="image/svg+xml">
+        <link rel="stylesheet" href="./assets/css/bulma.min.css">
     </head>
     <body>
         <section class="hero is-medium is-info is-bold">
