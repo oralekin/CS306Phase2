073888b8b7001479991cccd73da68111b8dbef35
Ekin Oral <oralekin@sabanciuniv.edu>
2025-05-13 21:53:38 +0300

    migrate to xampp stack running in docker

---
+++ scripts/sample/test_mongo.php
@@ -0,0 +1,37 @@
+<?php
+// Example file showing MongoDB connection using the native PHP MongoDB driver
+
+// Connect to MongoDB
+try {
+    // Create MongoDB client
+    $manager = new MongoDB\Driver\Manager("mongodb://" . $_ENV['MYSQL_ROOT_PASSWORD']. ":" . $_ENV['MYSQL_ROOT_PASSWORD']."@mongodb:27017");
+    
+    // Create a document to insert
+    $document = [
+        'name' => 'Test Document',
+        'date' => new MongoDB\BSON\UTCDateTime(time() * 1000)
+    ];
+    
+    // Create bulk write object and add our insert operation
+    $bulk = new MongoDB\Driver\BulkWrite;
+    $bulk->insert($document);
+    
+    // Execute the insert
+
+    $result = $manager->executeBulkWrite('app.app', $bulk);
+    
+    echo "Successfully connected to MongoDB and inserted a document!";
+    
+    // Query to verify our insert
+    $query = new MongoDB\Driver\Query([]);
+    $cursor = $manager->executeQuery('app.app', $query);
+    
+    echo "<pre>Documents in collection:<br>";
+    foreach ($cursor as $document) {
+        print_r($document);
+    }
+    echo "</pre>";
+    
+} catch (Exception $e) {
+    echo "Error connecting to MongoDB: " . $e->getMessage();
+}
