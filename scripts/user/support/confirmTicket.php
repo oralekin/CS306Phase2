994100b58595631529631c862f4d4bdf6b51cf14
Ekin Oral <oralekin@sabanciuniv.edu>
2025-05-21 13:56:40 +0300

    support: format confirmation when creating ticket

--- scripts/user/support/confirmTicket.php
+++ scripts/user/support/confirmTicket.php
@@ -9,9 +9,9 @@
 <a href="./createTicket.php">Create another ticket</a><br />
 <a href="..">Ticket list</a>
 <?php
-  $manager = new MongoDB\Driver\Manager("mongodb://" . getenv("MONGO_ROOT_USERNAME") . ":" . getenv("MONGO_ROOT_PASSWORD") . "@" . getenv("MONGO_URL") . ":27017");
-  $bulk = new MongoDB\Driver\BulkWrite();
-  if (!empty($_POST)) {
+$manager = new MongoDB\Driver\Manager("mongodb://" . getenv("MONGO_ROOT_USERNAME") . ":" . getenv("MONGO_ROOT_PASSWORD") . "@" . getenv("MONGO_URL") . ":27017");
+$bulk = new MongoDB\Driver\BulkWrite();
+if (!empty($_POST)) {
   $bulk->insert([
     'username'=> $_POST['username'],  
     'message'=> $_POST['body'],
@@ -21,11 +21,17 @@
   ]);
   $result = $manager->executeBulkWrite(getenv("MONGO_DATABASE") . ".tickets", $bulk);
 
-  $success = FALSE;
-  var_dump($result)
+  $success = $result->isAcknowledged();
+  if ($success) {
+    $messages = ['Inserted ' . $result->getInsertedCount() . ' document(s).'];
+  } else {
+    $messages = $result->getWriteErrors();
+  }
 ?>
-<h2>Ticket was <?= $success?"":"not "?> created successfully.</h2>
-<?= $message ?>
+  <h2>Ticket was <?= $success?"":"not "?> created successfully.</h2>
+  <?php             foreach ($messages as $message) { ?>
+    <?= $message ?><br />
+  <?php } ?>
 <?php } else { ?>
   <h2>Bad access.</h2>
 <?php } ?>
