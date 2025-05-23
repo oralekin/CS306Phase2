12b510574ea276dbb10bfbc867d130e76569492f
Ekin Oral <oralekin@sabanciuniv.edu>
2025-05-23 12:54:47 +0300

    update ticket details page titles

--- scripts/admin/ticketDetail/index.php
+++ scripts/admin/ticketDetail/index.php
@@ -112,7 +112,7 @@ switch ($_SERVER["REQUEST_METHOD"]) {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
+      <title>Admin Ticket Details</title>
     </head>
 
     <body>
--- scripts/user/support/ticketDetail/index.php
+++ scripts/user/support/ticketDetail/index.php
@@ -77,7 +77,7 @@ if (isset($_GET['id'])) {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ticket Details</title>
   </head>
 
   <body>
